8e15ba6e180875fa7112f47812f80576d8ad8d97
Ballursajeed <ballursajeed@gmail.com>
2023-12-16 05:43:57 +0530

    added .env file

--- db/index.js
+++ db/index.js
@@ -1,12 +1,9 @@
+import dotenv from 'dotenv';
 import mongoose from "mongoose"
 
 const connectDB= async()=>{
- try {
-  const connectionInstance = await mongoose.connect("mongodb+srv://ballursajeed:Welcome7867@cluster0.nllcgum.mongodb.net/sample-prac");
-  console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
- } catch (error) {
-  console.log("mongoDB connection FAILED:",error);
-  process.exit(1)
- }
+await mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected!!!"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 }
 export default connectDB
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 import { check, validationResult } from "express-validator";
 import connectDB from "./db/index.js";
